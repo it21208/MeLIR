66069a33d49ca6e8b84b26159fcaba51b83249d6
Alexandros Ioannidis <alexioanid@yahoo.gr>
2020-03-12 22:08:28 +0000

    new changes

--- scripts/main.py
+++ scripts/main.py
@@ -80,22 +80,20 @@ class main:
         flag = True
         learning_iterations = 5
         return(topic_qrels_dict, docs_reviewed, cols_seed, data_seed, batch, new_rows_seed, inverted_lookup_dict, doc_score, flag, learning_iterations, list_of_pmids_for_topic, training_set, tmp_lst, lookup_dict, topic_qrels_content_dict, ordered_feature_names)
-## if len(list_of_pmids_for_topic) > 0 and len(list_of_pmids_for_topic) <= 100: random_pmids = 50 # if len(list_of_pmids_for_topic) > 100 and len(list_of_pmids_for_topic) <= 1500: random_pmids = 300  # if len(list_of_pmids_for_topic) > 1500: random_pmids = 1000
+# if len(list_of_pmids_for_topic) > 0 and len(list_of_pmids_for_topic) <= 100: random_pmids = 50 # if len(list_of_pmids_for_topic) > 100 and len(list_of_pmids_for_topic) <= 1500: random_pmids = 300  # if len(list_of_pmids_for_topic) > 1500: random_pmids = 1000
+#4.Temporarily augment the training set by adding 100 random_pmids from the topic temporarily labeled 0 'not relevant', 5. Train an SVM classifier with training set. SVM is similar to LR. Apply LR to the train set.
     def augmentTrainSetWithRandomDocs_splitTraiTest_trainClf(self, list_of_pmids_for_topic, training_set, inverted_lookup_dict, topic, pathToFeatures, tmp_lst, clf, data_seed, new_rows_seed, doc_score, cols_seed, lookup_dict, vocab_idx_dict, tfidf_dict, ordered_feature_names):
         random_pmids = 100
         if random_pmids >= (len(list_of_pmids_for_topic) // 2):  random_pmids = len(list_of_pmids_for_topic) // 10
         random_docs = random.sample([x for x in list_of_pmids_for_topic if x not in list(training_set)], random_pmids)
-        # 4. Temporarily augment the training set by adding 100 random_pmids docs from the collection temporarily labeled 0 'not relevant'.
         for doc in random_docs: training_set.update({doc: 0})
-        # Initialise some empty temporary lists & Get a list of the train & test PMIDs in the training & test set. 
-        training_set_list_of_pmids, X_train, y_train, new_train_set, temp_list, X_testANDy_test, train_row, train_col, train_value, test_row, test_col, test_value, y_test, test_set = ([] for i in range(14))
+        # Initialise some empty temporary lists & Get a list of the train & test PMIDs in the training & test set.
+        training_set_list_of_pmids,X_train,y_train,new_train_set,temp_list,X_testANDy_test,train_row,train_col,train_value,test_row,test_col,test_value,y_test,test_set = ([] for i in range(14))
         training_set_list_of_pmids = [inverted_lookup_dict[i] for i in training_set]
         X_train = [key for key, value in training_set.items()]
-        y_train = [value for key, value in training_set.items()]
         X_test = np.setdiff1d(list_of_pmids_for_topic, random_docs)
         # Sparse matrix TF-IDF encoding to represent normalized word frequency scores in a vocabulary, reading both (train and test)X & (labels)y. So Train & Test have to split.
         cx, y = load(topic, pathToFeatures)
-        y_train=[]
         test_set = [str(x) for x in np.nditer(X_test)]
         new_train_set = [key for key, value in training_set.items()]
         # Build training and testing data with TF-IDF vectors
@@ -104,7 +102,8 @@ class main:
         counter = 0
         for i, j, v in zip(cx.row, cx.col, cx.data):
             try:
-                if lookup_dict[i] in new_train_set:  y_train_dict[i] = y[i]
+                if lookup_dict[i] in new_train_set:
+                    y_train_dict[i] = y[i]
                 y_test_dict[i] = y[i]
                 # Checks if docid is in train set.
                 if i in training_set_list_of_pmids:
@@ -123,7 +122,7 @@ class main:
         if max(list(np.unique(test_col))) > max(list(np.unique(train_col))): new_cols_seed, N, data_seed, temp_var = addCorrectColumnNumsForSeed(vocab_idx_dict, ordered_feature_names, test_col)
         else: new_cols_seed, N, data_seed, temp_var = addCorrectColumnNumsForSeed(vocab_idx_dict, ordered_feature_names, train_col)
         for i in new_cols_seed:
-          if max_j < i: max_j = i
+            if max_j < i: max_j = i
         #saveQueryTermIDsAndTFIDFValues(new_cols_seed, data_seed)
         # Reduce size of rows and data lists by N elements to match the length of the column. The values of the list of rows are assigned the doc num of the topic +1 because it's the seed.
         new_rows_seed = [new_rows_seed[0]]*len(new_cols_seed)
@@ -134,31 +133,29 @@ class main:
         X_test_col = np.array(test_col + list(new_cols_seed))
         x_test_value = np.array(test_value + data_seed)
         col_size_shape = max_j+1
-        # Get the size for the features of the train and test sparce matrices to be declared. 
+        # Get the size for the features of the train and test sparce matrices to be declared.
         if max_j+1 < max(list(np.unique(X_train_col)))+1 or max_j+1 < max(list(np.unique(X_test_col)))+1:
-            if max(list(np.unique(X_train_col)))+1 < max(list(np.unique(X_test_col)))+1:  col_size_shape = max(list(np.unique(X_test_col)))+1
+            if max(list(np.unique(X_train_col)))+1 < max(list(np.unique(X_test_col)))+1: col_size_shape = max(list(np.unique(X_test_col)))+1
             else: col_size_shape = max(list(np.unique(X_train_col)))+1
         # Train csr_matrix Must Have The Same Number of features/Cols the Test csr_matrix will have. - # Construct Train & Test csr matrix.
-        new_X_train = csr_matrix((X_train_value, (X_train_row, X_train_col)), shape=(max(list(np.unique(X_train_row)))+1, col_size_shape)) # save_npz("/home/pfb16181/NetBeansProjects/new_X_train.npz", new_X_train) 
-        new_X_test = csr_matrix((x_test_value, (X_test_row, X_test_col)), shape=(max(list(np.unique(X_test_row)))+1, col_size_shape)) #save_npz("/home/pfb16181/NetBeansProjects/new_X_test.npz", new_X_test)
+        new_X_train = csr_matrix((X_train_value, (X_train_row, X_train_col)), shape=(max(list(np.unique(X_train_row)))+1, col_size_shape))  # save_npz("/home/pfb16181/NetBeansProjects/new_X_train.npz", new_X_train)
+        new_X_test = csr_matrix((x_test_value, (X_test_row, X_test_col)), shape=(max(list(np.unique(X_test_row)))+1, col_size_shape))  # save_npz("/home/pfb16181/NetBeansProjects/new_X_test.npz", new_X_test)
         # Create the y_train labels for new_X_train.
         new_y_train = [0]*(new_X_train.shape[0])
         for key, value in collections.OrderedDict(zip(np.unique(X_train_row), y_train)).items(): new_y_train[key] = value
-        # Set the label of the Seed document to be 1 'relevant'. 
-        new_y_train[-1] = 1
-        # 5. Train an SVM classifier with training set. SVM is similar to LR. Apply LR to the train set. SVMlight was used with default parameters.
+        new_y_train[-1] = 1 # Set the label of the Seed document to be 1 'relevant'.
         y_test, new_p = evaluate_topic(new_X_train, new_y_train, new_X_test, clf)
         y_test = y_test.tolist()
         # Combine y_test and X_test in a data structure. If there is an exception it's because of the seed document.
-        try:  temp_list = [[lookup_dict[index], lst] for index, lst in enumerate(y_test)]
-        except: pass  
+        try: temp_list = [[lookup_dict[index], lst] for index, lst in enumerate(y_test)]
+        except: pass
         # Get scores for each doc using an ordered dictionary & updating it in each loop. The doc_score after each topic is sent back to run.py & stored in a list & then reinitialised for next topic.
         for i in temp_list: doc_score.update({i[0]: i[1]})
         # Order X_testANDy_test list or Order X_testANDy_test list based on the 2nd elements of the inner sublists if prediction log probabilities or decision function values available.
         if new_p is not None:
-          temp_y_test = [y for (p, y) in sorted(zip(new_p, temp_list), key=lambda pair: pair[0])]
-          temp_y_test.reverse()
-          temp_list = temp_y_test
+            temp_y_test = [y for (p, y) in sorted(zip(new_p, temp_list), key=lambda pair: pair[0])]
+            temp_y_test.reverse()
+            temp_list = temp_y_test
         else: temp_list = Sort_list_using_2nd_element_of_sublists(temp_list)
         # Remove from X_testANDy_test list the elements from the training_set. Here I am checking if it's in the testing set by checking if it's not in the training set.
         X_testANDy_test = [i for i in temp_list if i[0] not in list(training_set.keys())]
